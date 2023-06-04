27a9c4c947d030e9ef79d77e507313d74f304d45
Sushant S Samuel <iam@robosushie.com>
2023-06-04 14:34:14 +0530

    SEO changes 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import '@/styles/globals.css';
 
 export const metadata = {
-  title:
-    'Shreyansh Dubey - Data Enthusiast | Code Writer | Road Safety Advocate',
-  description: `"I'm a data enthusiast with a passion for problem-solving and a love for simplicity. Explore my expertise in coding, libraries such as sklearn, spacy, numpy, nltk, pandas, matplotlib, seaborn, opencv, keras, and tensorflow. Discover my dedication to promoting road safety for a safer community.`,
+  title: 'Shreyansh Dubey - Data Science Enthusiast | Road Safety',
+  description: `"I'm a passionate data science enthusiast. Explore my expertise in coding, libraries such as sklearn, spacy, numpy, nltk, pandas, seaborn, opencv, keras, and tensorflow. Discover my dedication to promoting road safety for a safer community.`,
   keywords: `data enthusiast, problem-solving, simplicity, code writer, sklearn, spacy, numpy, nltk, pandas, matplotlib, seaborn, opencv, keras, tensorflow, road safety advocate`
 };
 
